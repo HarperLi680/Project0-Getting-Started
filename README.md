90c4315d0aa824d69ca46e81a6e3f2d58bc35a0f
Qingying Li <qyinng@engineering.upenn.edu>
2026-09-11 01:17:14 +0000

    Save Project 0

--- README.md
+++ README.md
@@ -3,11 +3,51 @@ Project 0 Getting Started
 
 **University of Pennsylvania, CIS 5650: GPU Programming and Architecture, Project 0**
 
-* (TODO) YOUR NAME HERE
-  * (TODO) [LinkedIn](), [personal website](), [twitter](), etc.
-* Tested on: (TODO) Windows 22, i7-2222 @ 2.22GHz 22GB, GTX 222 222MB (Moore 2222 Lab)
+* Qingying Li
+  * [LinkedIn](https://www.linkedin.com/in/harper-li-292730373/)
+* Tested on: MacBook Pro, Apple M1 Pro, macOS, Google Chrome (WebGL/WebGPU); lab-provided NVIDIA H200 server, Ubuntu 22.04 Docker container, CUDA Toolkit 12.4, CMake 3.22.1 (CUDA).
 
-### (TODO: Your README)
 
-Include screenshots, analysis, etc. (Remember, this is public, so don't put
-anything here that you don't want to share with the world.)
+### CUDA GL Check
+
+I built and ran the application in a Docker container with access to
+one NVIDIA H200 GPU, using CUDA 12.4 and CMake 3.22.1.
+The GPU has compute capability 9.0.
+
+I used Xvfb and VirtualGL for remote display. The application shows
+blue and white regions, with my name and GPU information in the window title.
+
+![CUDA GL Check](images/cuda-gl-check.png)
+
+### CUDA Debugging
+
+I used Nsight Visual Studio Code Edition to set a breakpoint in
+kernel.cu and inspect the GPU thread's local variables.
+
+![CUDA debugging](images/cuda-debug.png)
+
+### Nsight Systems
+
+I collected a 15-second trace with Nsight Systems and viewed
+the Analysis Summary and Timeline. The trace recorded three
+calls to createVersionVisualization, with an average GPU
+execution time of about 4.18 microseconds. This small sample
+does not represent steady-state performance.
+
+![Nsight Systems summary](images/nsight-systems-summary.png)
+
+![Nsight Systems timeline](images/nsight-systems-timeline.png)
+
+### WebGL
+
+I checked WebGL in Chrome on my MacBook Pro. The report confirmed
+WebGL 1 support and listed Apple M1 Pro in the renderer information.
+
+![WebGL report](images/webgl.png)
+
+### WebGPU
+
+I checked WebGPU in Chrome on the same Mac. The report detected
+an Apple adapter and displayed its supported features and limits.
+
+![WebGPU report](images/webgpu.png)
--- cuda-gl-check/src/main.cpp
+++ cuda-gl-check/src/main.cpp
@@ -1,3 +1,5 @@
+#include <cstdlib>
+#define DIAG_CUDA(call) do { cudaError_t e = (call); if (e != cudaSuccess) { fprintf(stderr, "%s failed: %s\n", #call, cudaGetErrorString(e)); std::exit(1); } } while (0)
 #include <cstdio>
 #include <sstream>
 #include <GL/glew.h>
@@ -5,13 +7,14 @@
 #include <cuda_runtime_api.h>
 #include <cuda_gl_interop.h>
 #include "main.hpp"
+static cudaGraphicsResource* project0Resource = nullptr;
 
 /**
  * C main function.
  */
 int main(int argc, char* argv[]) {
     // TODO: Change this line to use your name!
-    m_yourName = "TODO: YOUR NAME HERE";
+    m_yourName = "Qingying Li";
 
     if (init(argc, argv)) {
         mainLoop();
@@ -60,7 +63,7 @@ bool init(int argc, char **argv) {
     glfwSetKeyCallback(m_window, keyCallback);
 
     glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
+    GLenum glewStatus = glewInit(); if (glewStatus != GLEW_OK && !(glewStatus == GLEW_ERROR_NO_GLX_DISPLAY && glGetString(GL_VERSION) != nullptr && GLEW_VERSION_4_0)) { fprintf(stderr, "GLEW initialization failed: %s\n", glewGetErrorString(glewStatus)); fprintf(stderr, "GLEW error code: %u\n", (unsigned)glewStatus);
         return false;
     }
 
@@ -91,7 +94,7 @@ void initPBO(GLuint *pbo) {
         glBindBuffer(GL_PIXEL_UNPACK_BUFFER, *pbo);
         // Allocate data for the buffer. 4-channel 8-bit image
         glBufferData(GL_PIXEL_UNPACK_BUFFER, size_tex_data, NULL, GL_DYNAMIC_COPY);
-        cudaGLRegisterBufferObject(*pbo);
+        DIAG_CUDA(cudaGraphicsGLRegisterBuffer(&project0Resource, *pbo, cudaGraphicsRegisterFlagsNone));
     }
 }
 
@@ -132,7 +135,7 @@ void initVAO() {
 void initCUDA() {
     // Default to device ID 0. If you have more than one GPU and want to test a non-default one,
     // change the device ID.
-    cudaGLSetGLDevice(0);
+    DIAG_CUDA(cudaSetDevice(0));
 }
 
 void initTextures() {
@@ -163,29 +166,97 @@ void runCUDA() {
     // Map OpenGL buffer object for writing from CUDA on a single GPU
     // No data is moved (Win & Linux). When mapped to CUDA, OpenGL should not use this buffer
     uchar4 *dptr = NULL;
-    cudaGLMapBufferObject((void**)&dptr, m_pbo);
+    size_t mappedBytes = 0; DIAG_CUDA(cudaGraphicsMapResources(1, &project0Resource, 0)); DIAG_CUDA(cudaGraphicsResourceGetMappedPointer((void**)&dptr, &mappedBytes, project0Resource));
 
     // Execute the kernel
     kernelVersionVis(dptr, m_width, m_height, m_major, m_minor);
 
     // Unmap buffer object
-    cudaGLUnmapBufferObject(m_pbo);
+    DIAG_CUDA(cudaGraphicsUnmapResources(1, &project0Resource, 0));
 }
 
 void mainLoop() {
+    fprintf(stderr, "Entered rendering loop\n");
     while (!glfwWindowShouldClose(m_window)) {
+        static bool traceFirst = true; if (traceFirst) fprintf(stderr, "STEP 1: before events\n");
         glfwPollEvents();
+        if (traceFirst) fprintf(stderr, "STEP 2: before CUDA\n");
         runCUDA();
+        // PROJECT0_WINDOW_RECONNECT: restore the window context after CUDA.
+        glfwMakeContextCurrent(nullptr);
+        glfwMakeContextCurrent(m_window);
+        if (traceFirst) fprintf(stderr, "STEP 3: CUDA finished\n");
 
         glBindBuffer(GL_PIXEL_UNPACK_BUFFER, m_pbo);
+        {
+            static bool checkedPbo = false;
+            if (!checkedPbo) {
+                checkedPbo = true;
+                unsigned char a[4] = {}, b[4] = {};
+                glGetBufferSubData(GL_PIXEL_UNPACK_BUFFER,
+                    (200 * m_width + 400) * 4, 4, a);
+                glGetBufferSubData(GL_PIXEL_UNPACK_BUFFER,
+                    (600 * m_width + 400) * 4, 4, b);
+                fprintf(stderr, "PBO check: error=0x%x RGB1=%u,%u,%u RGB2=%u,%u,%u\n",
+                    glGetError(), a[0], a[1], a[2], b[0], b[1], b[2]);
+            }
+        }
         glBindTexture(GL_TEXTURE_2D, m_image);
         glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, m_width, m_height, GL_RGBA,
                         GL_UNSIGNED_BYTE, NULL);
         glClear(GL_COLOR_BUFFER_BIT);
+        {
+            static bool drawStateChecked = false; // DRAW_STATE_CHECK
+            if (!drawStateChecked) {
+                drawStateChecked = true;
+                GLint program = 0, linked = 0, width = 0, height = 0;
+                glGetIntegerv(GL_CURRENT_PROGRAM, &program);
+                if (program) glGetProgramiv(program, GL_LINK_STATUS, &linked);
+                glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &width);
+                glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &height);
+                fprintf(stderr, "DRAW: program=%d linked=%d texture=%dx%d\n",
+                    program, linked, width, height);
+                if (program) fprintf(stderr, "ATTR: Position=%d Texcoords=%d\n",
+                    glGetAttribLocation(program, "Position"),
+                    glGetAttribLocation(program, "Texcoords"));
+
+                if (width == 800 && height == 800) {
+                    GLint packBuffer = 0;
+                    glGetIntegerv(GL_PIXEL_PACK_BUFFER_BINDING, &packBuffer);
+                    glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+                    unsigned char* pixels = new unsigned char[800 * 800 * 4]();
+                    glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+                    unsigned a = (200 * 800 + 400) * 4;
+                    unsigned b = (600 * 800 + 400) * 4;
+                    fprintf(stderr, "TEXTURE: error=0x%x RGB1=%u,%u,%u RGB2=%u,%u,%u\n",
+                        glGetError(), pixels[a], pixels[a+1], pixels[a+2],
+                        pixels[b], pixels[b+1], pixels[b+2]);
+                    delete[] pixels;
+                    glBindBuffer(GL_PIXEL_PACK_BUFFER, packBuffer);
+                }
+            }
+        }
 
         // VAO, shader program, and texture already bound
+        if (traceFirst) fprintf(stderr, "STEP 4: before draw\n");
+        glViewport(0, 0, m_width, m_height);
         glDrawElements(GL_TRIANGLES, 6,  GL_UNSIGNED_SHORT, 0);
+        if (traceFirst) fprintf(stderr, "STEP 5: draw returned\n");
+        {
+            static bool checkedFrame = false;
+            if (!checkedFrame) {
+                checkedFrame = true;
+                GLenum drawError = glGetError();
+                unsigned char a[4] = {}, b[4] = {};
+                glReadBuffer(GL_BACK);
+                glReadPixels(400, 200, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, a);
+                glReadPixels(400, 600, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, b);
+                fprintf(stderr, "Frame check: drawError=0x%x readError=0x%x RGB1=%u,%u,%u RGB2=%u,%u,%u\n",
+                    drawError, glGetError(), a[0], a[1], a[2], b[0], b[1], b[2]);
+            }
+        }
         glfwSwapBuffers(m_window);
+        if (traceFirst) fprintf(stderr, "STEP 6: frame finished\n"); traceFirst = false;
     }
     cleanupCUDA();
     glfwDestroyWindow(m_window);
@@ -219,7 +290,7 @@ void cleanupCUDA() {
 void deletePBO(GLuint *pbo) {
     if (pbo) {
         // unregister this buffer object with CUDA
-        cudaGLUnregisterBufferObject(*pbo);
+        DIAG_CUDA(cudaGraphicsUnregisterResource(project0Resource)); project0Resource = nullptr;
 
         glBindBuffer(GL_ARRAY_BUFFER, *pbo);
         glDeleteBuffers(1, pbo);
